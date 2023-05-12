a9b6dba8dd627b8bca33730ebb6bdcec7d72b75a
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-05-12 22:04:54 +0200

    Add markdown support

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
 import { useEffect, useRef } from "react";
-import { EditorView, EditorViewConfig } from "@codemirror/view";
-import { minimalSetup} from "codemirror";
-
+import { EditorView, EditorViewConfig, keymap } from "@codemirror/view";
+import { minimalSetup } from "codemirror";
+import { indentWithTab } from "@codemirror/commands";
+import { markdown } from "@codemirror/lang-markdown";
 
 const initialSource = `- Foo
-    -bar
-    -baz
-      - more
-    -lol
-`
+  -bar
+  -baz
+    - more
+  -lol
+`;
 
 function App() {
-  const containerRef = useRef<HTMLDivElement|null>(null)
-  const editorViewRef = useRef<EditorView>()
-
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const editorViewRef = useRef<EditorView>();
 
   useEffect(() => {
     if (!containerRef.current) {
-      return
+      return;
     }
 
-    const view = editorViewRef.current = new EditorView({
+    const view = (editorViewRef.current = new EditorView({
       doc: initialSource,
       extensions: [
         minimalSetup,
         EditorView.lineWrapping,
+        markdown(),
+        keymap.of([indentWithTab]),
       ],
       parent: containerRef.current,
-    } as EditorViewConfig)
+    } as EditorViewConfig));
 
     return () => {
-      view.destroy()
-    }
-  }, [])
+      view.destroy();
+    };
+  }, []);
 
-
-  return <div ref={containerRef}></div>
-
+  return <div ref={containerRef}></div>;
 }
 
-export default App
+export default App;
